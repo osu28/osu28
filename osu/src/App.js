727bfda351f40be223996352ff592a1ddefe830b
osu28 <osu@purdue.edu>
2022-07-19 20:07:46 -0400

    frosted glass

--- osu/src/App.js
+++ osu/src/App.js
@@ -6,18 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Oscar Su
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = "container">
+          <div className = "profile">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <p>Hello 👋</p>
+            <p>My name is:</p>
+            <h1>Oscar Su</h1>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </div>
+        </div>
       </header>
     </div>
   );
