fcdfb19b4c19b403584e95d21b73d2521091a912
osu28 <osu@purdue.edu>
2022-08-05 01:28:49 -0400

    updated experience

--- osu/src/components/Experience.js
+++ osu/src/components/Experience.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export const Experience = () => {
     return (
-      <p>My Experience</p>
+      <div>
+        <h3>Adaptive Biotechnologies</h3>
+        <p>Software Engineer Intern, <i>Summer 2022</i></p>
+        <h3>CACI</h3>
+        <p>Machine Learning Intern, <i>Spring 2022</i></p>
+        <p>Software Engineer Intern, <i>Summer 2021</i></p>
+      </div>
     );
   };
